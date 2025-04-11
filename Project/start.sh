7f513f699955331aad2bbaa157caf3220b43fa0c
Prosperity <53616398+Prisoe@users.noreply.github.com>
2025-04-10 22:17:14 -0400

    Delete Project directory

--- Project/start.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Start Flask (port 5000) in background
-python Backend/app.py &
-
-# Start Streamlit (port 8501)
-streamlit run Frontend/App.py --server.port 8501 --server.enableCORS false
